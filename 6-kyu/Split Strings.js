24830bdb70e38308762aa3870da7b0252c5a8db5
Cheng Wu <112827606+Cheng57@users.noreply.github.com>
2024-05-21 04:32:37 -0500

    Update Split Strings.js
    Add Reflection

--- 6-kyu/Split Strings.js
+++ 6-kyu/Split Strings.js
@@ -6,6 +6,15 @@ If the string contains an odd number of characters then it should replace the mi
 Examples:
 'abc' => ['ab', 'c_']
 'abcdef' => ['ab', 'cd', 'ef']
+
+Reflection:
+I add an underscore to the string and then slice the string into pairs of two characters.
+
+The indexEnd in the 'slice' method is set to i and i is defaulted to 2 so that the 'for' iteration terminates when 
+
+i is bigger than the length of the string. This promises the result array will not include the '_' as a pair when 
+
+the string contains an even number of characters. 
 */
 
 function solution(str){
